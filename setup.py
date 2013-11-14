b2ecccb3e8a83a4263cba4a24552b7e8b062e34a
Daniel Watkins <daniel.watkins@hogarthww.com>
2013-11-14 16:09:22 +0000

    Bump to v1.0.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pylint-nose-checker",
-    version="0.0.1",
+    version="1.0.0",
     url='http://github.com/OddBloke/pylint-nose-checker',
     license='GPLv3',
     description="A pylint plugin which fixes nose.tools import errors",
